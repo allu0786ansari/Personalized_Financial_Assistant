eacc2572a412e6217cde6d6a2cd3c765c6a5f1f1
Allaudin Ansari <allu456654ansari@gmail.com>
2024-10-11 15:58:04 +0530

    setting updated

--- financial_assistant/settings.py
+++ financial_assistant/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,11 +84,11 @@ WSGI_APPLICATION = 'financial_assistant.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'personalized_financial_assistant',
-        'USER': 'root',
-        'PASSWORD': 'A6VLT@$cuDYA.4n', #set your database password
-        'HOST': 'localhost',  # Set to your database host
-        'PORT': '3306',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='3306'),
     }
 }
 
